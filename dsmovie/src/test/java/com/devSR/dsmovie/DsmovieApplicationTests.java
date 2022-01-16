0b5f330342c4ba5e371edff65151322fa46ae44e
Tiago Ribeiro <tiagovrib@gmail.com>
2022-01-16 15:28:57 +0000

    backend sr-movie

---
+++ dsmovie/src/test/java/com/devSR/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSR.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
